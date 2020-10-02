0347de120c0f1de2362bc51108dedce4887ea26a
Jefferson <jeff.venceslau@gmail.com>
2020-10-02 18:59:49 -0300

    ok

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
   <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Vamos Uploadingar</title>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-		<link rel="stylesheet"  href="style.css"/>
-	</head>
-	<body>
-		<div class="area-upload">
-			<label for="upload-file" class="label-upload">
-				<i class="fas fa-cloud-upload-alt"></i>
-				<div class="texto"> </div>
-			</label>
-			<input type="file" id="upload-file" multiple/>
-			<div class="lista-uploads">
-			</div>
-		</div>
-		<script src="script.js"></script>
-	</body>
-</html>
+  <html>
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vamos Uploadingar</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  <body>
+    <div class="area-upload">
+      <label for="upload-file" class="label-upload">
+        <i class="fas fa-cloud-upload-alt"></i>
+        <div class="texto"> "Upload"</div>
+      </label>
+      <input type="file" id="upload-file" multiple />
+      <div class="lista-uploads">
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+
+  </html>
